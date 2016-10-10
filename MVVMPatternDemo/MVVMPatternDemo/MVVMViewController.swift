2a257b710aa0b0ca5862b735e1a6554db988aa7e
forwardto9 <forwardto9@uwei-macbook.lan>
2016-10-11 00:32:14 +0800

    modify to easy

--- MVVMPatternDemo/MVVMPatternDemo/MVVMViewController.swift
+++ MVVMPatternDemo/MVVMPatternDemo/MVVMViewController.swift
@@ -19,22 +19,25 @@ protocol ViewModelProtocol:class {
     var greeting:String? {get}
     
     // hook by block
-    var greetingDidChange:((ViewModelProtocol) -> ())? {get set}
+//    var greetingDidChange:((_ vModel:ViewModelProtocol) -> ())? {get set}
+    var greetingDidChange:(() -> Void)? {get set}
     init(person:Person)
     func showGreeting() -> Void
 }
 
 // 独立出来的模块
-// 负责 数据的逻辑处理 + UI操作
+// 负责 数据的逻辑处理
 class ViewModel: ViewModelProtocol {
     let person:Person
     var greeting: String? {
         didSet {
-            self.greetingDidChange?(self)
+//            self.greetingDidChange?(self)
+            self.greetingDidChange?()
         }
     }
     
-    var greetingDidChange: ((ViewModelProtocol) -> ())?
+//    var greetingDidChange: ((_ vModel:ViewModelProtocol) -> ())?
+    var greetingDidChange: (() -> Void)?
     required init(person: Person) {
         self.person = person
     }
@@ -46,21 +49,21 @@ class ViewModel: ViewModelProtocol {
 
 
 class MVVMViewController: UIViewController {
-
-//    private let person = Person(name: "uwei", age: 27);
-    
     // 也是可以的，但是这只能将数据的逻辑处理放到了控制器中，增加了耦合，而且在响应UI操作的时候，也只能放到控制器中，耦合性大增
     // 数据和UI的处理绑定在一起
+/*
     var person:Person! {
         didSet {
             self.label.text = "Hi" + " " + person.name
         }
     }
+ */
     
     var viewModel:ViewModelProtocol! {
         didSet {
-            self.viewModel.greetingDidChange = { [unowned self] viewModel in
-                self.label.text = viewModel.greeting
+//            self.viewModel.greetingDidChange = { [unowned self] viewModel in
+            self.viewModel.greetingDidChange = {
+                self.label.text = self.viewModel.greeting
             }
         }
     }
@@ -77,15 +80,15 @@ class MVVMViewController: UIViewController {
         let frame = self.view.frame
         
         button.frame = CGRect(x:frame.width/2 - 32, y:frame.height/2 - 22, width:64, height:44)
-        button.backgroundColor = UIColor.black()
+        button.backgroundColor = UIColor.black
         button.setTitle("Set", for: UIControlState.selected)
-//        button.addTarget(self, action: #selector(self.showGreeting), forControlEvents: UIControlEvents.TouchUpInside)
         button.addTarget(viewModel, action: #selector(ViewModel.showGreeting), for: UIControlEvents.touchUpInside)
         self.view.addSubview(button)
         
         label.frame  = CGRect(x:frame.width/2 - 60, y:frame.height/2 - 50, width:120, height:40)
-        label.backgroundColor = UIColor.blue()
-        label.textColor = UIColor.brown()
+        label.backgroundColor = UIColor.blue
+        label.textColor = UIColor.white
+        label.text      = "Default"
         self.view.addSubview(label)
 
     }
@@ -94,22 +97,4 @@ class MVVMViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    func showGreeting() -> Void {
-//        label.text = "Hi" + " " + person.name
-    }
-    
-    
-    
 }
